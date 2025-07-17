ac6df2799b4e72e211112327e5173b8a0b9d3a25
teejeedeeone2025 <teejeedeeone2025@gmail.com>
2025-07-17 06:55:18 +0100

    Update url_monitor.py

--- url_monitor.py
+++ url_monitor.py
@@ -12,6 +12,7 @@ from github import Github
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import chromedriver_autoinstaller
 
